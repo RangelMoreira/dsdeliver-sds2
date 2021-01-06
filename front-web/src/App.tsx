a5f199ed6d88f22d282751ac11e77655fc88a4c0
Kevin Rangel Moreira <rangel.moreira@outlook.com>
2021-01-06 12:14:17 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,11 @@
 // import React from 'react';
 import './App.css';
+import Navbar from './Navbar/Index';
 
 
 function App() {
   return (
-    <div >
-      <h1>Hello</h1>
-    </div>
+   <Navbar/>
   );
 }
 
